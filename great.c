557d9127071242fc55529b08ef757082720b913e
kunavathi <gunasaindhu@gmail.com>
2016-07-10 11:24:02 +0530

    finish

---
+++ great.c
@@ -0,0 +1,19 @@
+# guna 2
+#include<stdio.h>
+{
+int a[N];
+int num,i,x;
+for(i=1;i<5;i++)
+{
+num=a[0];
+if(num>a[i])
+{
+num=a[i];
+}
+else 
+{
+x=num;
+}
+printf("the descending order is",x);
+}
+}
